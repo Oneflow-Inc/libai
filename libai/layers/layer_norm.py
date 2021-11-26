03a9eb30dad9b60029d6a3538a817df07bf80d9b
l1aoxingyu <sherlockliao01@gmail.com>
2021-11-26 17:19:13 +0800

    add elementwise_affine argument in layernorm

--- libai/layers/layer_norm.py
+++ libai/layers/layer_norm.py
@@ -25,44 +25,69 @@ class LayerNorm(nn.Module):
     Arguments:
         normalized_shape: input shape from an expected input of size.
         eps: a value added to the denominator for numerical stability. Defaults to 1e-5.
+        elementwise_affine: a boolean value that when set to ``True``, this module
+        has learnable per-element affine parameters initialized to ones (for weights) and zeros (for biases). Default: ``True``.
+
         layer_idx: A layer_idx sign which determines the placement. It will be used in pipeline parallelism. Defaults to 0.
     """
 
-    def __init__(self, normalized_shape, eps=1e-5, *, layer_idx=0):
+    def __init__(
+        self,
+        normalized_shape,
+        eps=1e-5,
+        elementwise_affine: bool = True,
+        *,
+        layer_idx=0
+    ):
         super().__init__()
-        self.normalized_shape = normalized_shape
-        self.epsilon = eps
+        if isinstance(normalized_shape, int):
+            normalized_shape = (normalized_shape,)
+        self.normalized_shape = tuple(normalized_shape)
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
 
-        self.gamma = flow.nn.Parameter(
-            flow.ones(
-                normalized_shape,
-                dtype=flow.float32,
-                placement=dist.get_layer_placement(layer_idx),
-                sbp=dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.broadcast]),
+        if elementwise_affine:
+            self.weight = flow.nn.Parameter(
+                flow.ones(
+                    normalized_shape,
+                    dtype=flow.float32,
+                    placement=dist.get_layer_placement(layer_idx),
+                    sbp=dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.broadcast]),
+                )
             )
-        )
 
-        self.beta = flow.nn.Parameter(
-            flow.zeros(
-                normalized_shape,
-                dtype=flow.float32,
-                placement=dist.get_layer_placement(layer_idx),
-                sbp=dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.broadcast]),
+            self.bias = flow.nn.Parameter(
+                flow.zeros(
+                    normalized_shape,
+                    dtype=flow.float32,
+                    placement=dist.get_layer_placement(layer_idx),
+                    sbp=dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.broadcast]),
+                )
             )
-        )
+        else:
+            self.weight = None
+            self.bias = None
 
     def forward(self, x):
         assert x.shape[-len(self.normalized_shape) :] == self.normalized_shape
         begin_norm_axis = x.ndim - len(self.normalized_shape)
         begin_params_axis = x.ndim - len(self.normalized_shape)
-        y = flow._C.layer_norm_affine(
-            x,
-            self.gamma,
-            self.beta,
-            begin_norm_axis=begin_norm_axis,
-            begin_params_axis=begin_params_axis,
-            epsilon=self.epsilon,
-        )
+        if self.elementwise_affine:
+            y = flow._C.layer_norm_affine(
+                x,
+                self.weight,
+                self.bias,
+                begin_norm_axis=begin_norm_axis,
+                begin_params_axis=begin_params_axis,
+                epsilon=self.epsilon,
+            )
+        else:
+            y = flow._C.layer_norm(
+                x,
+                begin_norm_axis=self.begin_norm_axis,
+                begin_params_axis=self.begin_params_axis,
+                epsilon=self.eps,
+            )
         return y
 
     def extra_repr(self) -> str:
