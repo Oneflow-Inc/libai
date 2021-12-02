e4bf6c49d2ea788180d4ecbccd56c8d0c1c0ec73
Xingyu Liao <sherlockliao01@gmail.com>
2021-12-02 21:27:59 +0800

    Feat/add linear (#5)
    Co-authored-by: dangkai <dangkai4u@outlook.com>

---
+++ libai/layers/linear.py
@@ -0,0 +1,152 @@
+# coding=utf-8
+# Copyright 2021 The OneFlow Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import oneflow as flow
+from oneflow import nn
+
+from libai.utils import distributed as dist
+
+
+class Linear1D(nn.Module):
+    """Linear layer with 1D parallelism which includes column parallelism and row parallelism.
+    The linear layer is defined as :math:`Y = XA + b`.
+
+    In column parallelism, A is parallelized along the second dimension as :math:`A = [A_1, ..., A_p]`.
+    
+    In row parallelism, A is parallelized along the first dimension and X along its second dimension as:
+                | A_1 |
+                |  .  |
+            A = |  .  |         X = [X_1, ..., X_p]
+                |  .  |
+                | A_p |
+
+    Arguments:
+        in_features: size of each input sample.
+        out_features: size of each output sample.
+        bias: If set to ``False``, the layer will not learn an additive bias. Defaults to ``True``.
+        parallel: . Defaults to "data".
+        init_method: method to initialize weight. Defaults to nn.init.xavier_normal_.
+        skip_bias_add: skip adding bias but instead return it, so that adding bias can be fused with other elementwise operations. Defaults to ``False``.
+        layer_idx: A layer_idx sign which determines the placement. It will be used in pipeline parallelism. Defaults to 0.
+    """
+
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        bias=True,
+        parallel="data",
+        init_method=nn.init.xavier_normal_,
+        skip_bias_add=False,
+        *,
+        layer_idx=0,  # enforce layer_idx passed with keyword
+    ):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.parallel = parallel
+        self.skip_bias_add = skip_bias_add
+
+        if parallel == "col":
+            # Column parallel weight sbp: [B, S(1)] and bias sbp: [B, S(0)].
+            weight_sbp = dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.split(1)])
+            bias_sbp = dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.split(0)])
+        elif parallel == "row":
+            # Row parallel weight sbp: [B, S(0)] and bias sbp: [B, B]
+            weight_sbp = dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.split(0)])
+            bias_sbp = dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.broadcast])
+        elif parallel == "data":
+            weight_sbp = flow.sbp.broadcast
+            bias_sbp = flow.sbp.broadcast
+        else:
+            raise KeyError(
+                f"{parallel} is not supported! Only support ('data', 'row' and 'col')"
+            )
+
+        self.weight = flow.nn.Parameter(
+            flow.empty(
+                (in_features, out_features),
+                dtype=flow.float32,
+                placement=dist.get_layer_placement(
+                    layer_idx
+                ),  # for pipeline parallelism placement
+                sbp=weight_sbp,
+            )
+        )
+        init_method(self.weight)
+
+        self.bias = (
+            flow.nn.Parameter(
+                flow.zeros(
+                    (out_features,),
+                    dtype=flow.float32,
+                    placement=dist.get_layer_placement(layer_idx),
+                    sbp=bias_sbp,
+                )
+            )
+            if bias
+            else None
+        )
+
+    def forward(self, x):
+        if dist.same_sbp(
+            self.weight.sbp, dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.split(1)])
+        ):
+            # When weight sbp sign is [B, S(1)], change x sbp sign to [S(0), B].
+            x = x.to_consistent(
+                sbp=dist.get_nd_sbp([flow.sbp.split(0), flow.sbp.broadcast])
+            )
+
+            # Matmul sbp sign: [S(0), B] x [B, S(1)] -> [S(0), S(1)]
+            # Backward x.grad sbp sign: [S(0), S(1)] x [B, S(0)] (weight.T) -> [S(0), P]
+            # which cannot do backward pass when sbp sign is [S(0), P]
+            # so change x.grad sbp: [S(0), P] -> [S(0), B]
+            x = x.to_consistent(grad_sbp=x.sbp)
+            x = flow._C.matmul(x, self.weight)
+
+        elif dist.same_sbp(
+            self.weight.sbp, dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.split(0)])
+        ):
+            # When weight sbp sign is [B, S(0)], change x sbp sign to [S(0), S(1)].
+            x = x.to_consistent(
+                sbp=dist.get_nd_sbp([flow.sbp.split(0), flow.sbp.split(x.ndim - 1)])
+            )
+            # Matmul sbp sign: [S(0), S(1)] x [B, S(0)] -> [S(0), P]
+            # Backward x.grad sbp sign: [S(0), B] x [B, S(1)] (weight.T) -> [S(0), S(1)]
+            x = flow._C.matmul(x, self.weight)
+            # Change x sbp: [S(0), P] -> [S(0), B] for followup forward pass.
+            x = x.to_consistent(
+                sbp=dist.get_nd_sbp([flow.sbp.split(0), flow.sbp.broadcast])
+            )
+        else:
+            raise NotImplementedError(f"Not support weight with sbp: {self.weight.sbp}")
+
+        if self.bias is not None:
+            if self.skip_bias_add:
+                return x, self.bias
+            else:
+                return x + self.bias
+        else:
+            return x
+
+    def extra_repr(self) -> str:
+        return "in_features={}, out_features={}, bias={}, parallel={}".format(
+            self.in_features, self.out_features, self.bias is not None, self.parallel,
+        )
+
+
+# Give an alias for Linear1d
+Linear = Linear1D
---
+++ libai/layers/mlp.py
@@ -0,0 +1,112 @@
+# coding=utf-8
+# Copyright 2021 The OneFlow Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import oneflow as flow
+from oneflow import nn
+
+from libai.layers import Linear, build_activation
+
+
+class MLP(nn.Module):
+    """ MLP
+
+    MLP will take the input with h hidden state, project it to intermediate 
+    hidden dimension, perform gelu transformation, and project the
+    state back into h hidden dimension.
+    
+    Arguments:
+        hidden_size: size of each input and output sample.
+        ffn_hidden_size: size of each intermediate sample.
+        output_dropout_prob: Output dropout probability. Defaults to 0.0.
+        init_method: method to initialize the first linear weight. Defaults to nn.init.xavier_normal_.
+        output_layer_init_method: method to initialize the second linear weight. If set to None, 
+        it will use ``init_method`` instead. Defaults to None.
+        bias_gelu_fusion: If set to ``True``, it will fuse bias adding and elementwise gelu activation. 
+        Defaults to ``False``.
+        bias_dropout_fusion: If set to ``True``, it will fuse bias adding and dropout. Defaults to ``False``.
+        layer_idx: A layer_idx sign which determines the placement. It will be used in 
+        pipeline parallelism. Defaults to 0.
+    """
+
+    def __init__(
+        self,
+        hidden_size,
+        ffn_hidden_size,
+        output_dropout_prob=0.0,
+        init_method=nn.init.xavier_normal_,
+        output_layer_init_method=None,
+        bias_gelu_fusion=False,
+        bias_dropout_fusion=False,
+        *,
+        layer_idx=0,
+    ):
+        super().__init__()
+        self.output_dropout_prob = output_dropout_prob
+        self.bias_gelu_fusion = bias_gelu_fusion
+        self.bias_dropout_fusion = bias_dropout_fusion
+
+        if output_layer_init_method is None:
+            output_layer_init_method = init_method
+
+        self.dense_h_to_4h = Linear(
+            hidden_size,
+            ffn_hidden_size,
+            bias=True,
+            parallel="col",
+            skip_bias_add=bias_gelu_fusion,
+            init_method=init_method,
+            layer_idx=layer_idx,
+        )
+
+        if not bias_gelu_fusion:
+            self.activation_func = build_activation("gelu")
+
+        self.dense_4h_to_h = Linear(
+            ffn_hidden_size,
+            hidden_size,
+            bias=True,
+            parallel="row",
+            skip_bias_add=bias_dropout_fusion,
+            init_method=output_layer_init_method,
+            layer_idx=layer_idx,
+        )
+
+        if not bias_dropout_fusion:
+            self.dropout = nn.Dropout(self.output_dropout_prob)
+
+    def forward(self, hidden_states):
+        intermediate = self.dense_h_to_4h(hidden_states)
+        if self.bias_gelu_fusion:
+            intermediate, bias = intermediate
+            intermediate = flow._C.fused_bias_add_gelu(
+                intermediate, bias, axis=intermediate.ndim - 1
+            )
+        else:
+            intermediate = self.activation_func(intermediate)
+
+        output = self.dense_4h_to_h(intermediate)
+        if self.bias_dropout_fusion:
+            output, bias = output
+            output = flow._C.fused_bias_add_dropout(
+                output, bias, p=self.output_dropout_prob, axis=output.ndim - 1
+            )
+        else:
+            output = self.dropout(output)
+        return output
+
+    def extra_repr(self) -> str:
+        return "bias_gelu_fusion={}, bias_dropout_fusion={}, dropout={}".format(
+            self.bias_gelu_fusion, self.bias_dropout_fusion, self.output_dropout_prob
+        )
