09842dfca7891d1f918fd50cfec3cc2b67a6ef22
rentainhe <596106517@qq.com>
2021-12-28 12:11:52 +0800

    update scheduler

--- libai/solver/optimizer.py
+++ libai/solver/optimizer.py
@@ -1,3 +1,18 @@
+# coding=utf-8
+# Copyright 2021 The OneFlow Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Modified from https://github.com/facebookresearch/detectron2/blob/main/detectron2/solver/build.py
 """
@@ -8,8 +23,6 @@ import logging
 from collections import defaultdict
 from enum import Enum
 from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Type, Union
-# TODO: switch to libai.config.CfgNode
-from yacs.config import CfgNode
 
 import oneflow as flow
 import oneflow.nn as nn
@@ -21,27 +34,28 @@ class GradientClipType(Enum):
     VALUE = "value"
     NORM = "norm"
 
-def _create_gradient_clipper(cfg: CfgNode) -> _GradientClipper:
+def _create_gradient_clipper(cfg) -> _GradientClipper:
     """
     Creates gradient clipping closure to clip by value or by norm,
     according to the provided config.
     """
     cfg = copy.deepcopy(cfg)
 
+    # TODO: add optim.clip_gradient cfg into libai's default config file
     def clip_grad_norm(p: _GradientClipperInput):
-        nn.utils.clip_grad_norm_(p, cfg.CLIP_VALUE, cfg.NORM_TYPE)
+        nn.utils.clip_grad_norm_(p, cfg.optim.clip_gradient.clip_value, cfg.optim.clip_gradient.norm_type)
     
     def clip_grad_value(p: _GradientClipperInput):
-        nn.utils.clip_grad_value_(p, cfg.CLIP_VALUE)
+        nn.utils.clip_grad_value_(p, cfg.optim.clip_gradient.clip_value)
     
     _GRADIENT_CLIP_TYPE_TO_CLIPPER = {
         GradientClipType.VALUE: clip_grad_value,
         GradientClipType.NORM: clip_grad_norm,
     }
-    return _GRADIENT_CLIP_TYPE_TO_CLIPPER[GradientClipType(cfg.CLIP_TYPE)]
+    return _GRADIENT_CLIP_TYPE_TO_CLIPPER[GradientClipType(cfg.optim.clip_gradient.clip_type)]
 
 
-def _generate_optimizer_class_with_gradient_clippint(
+def _generate_optimizer_class_with_gradient_clipping(
     optimizer: Type[flow.optim.Optimizer],
     *,
     per_param_clipper: Optional[_GradientClipper] = None,
@@ -74,3 +88,39 @@ def _generate_optimizer_class_with_gradient_clippint(
         {"step": optimizer_wgc_step},
     )
     return OptimizerWithGradientClip
+
+
+def maybe_add_gradient_clipping(
+    cfg, optimizer: Type[flow.optim.Optimizer]
+) -> Type[flow.optim.Optimizer]:
+    """
+    If gradient clipping is enabled through config options, wraps the existing
+    optimizer type to become a new dynamically created class OptimizerWithGradientClip
+    that inherits the given optimizer and overrides the `step` method to
+    include gradient clipping.
+
+    Args:
+        cfg: configuration options
+        optimizer: type. A subclass of torch.optim.Optimizer
+
+    Return:
+        type: either the input `optimizer` (if gradient clipping is disabled), or
+            a subclass of it with gradient clipping included in the `step` method.
+    """
+    if not cfg.optim.clip_gradients.enable:
+        return optimizer
+    if isinstance(optimizer, flow.optim.Optimizer):
+        optimizer_type = type(optimizer)
+    else:
+        assert issubclass(optimizer, flow.optim.Optimizer), optimizer
+        optimizer_type = optimizer
+
+    grad_clipper = _create_gradient_clipper(cfg.SOLVER.CLIP_GRADIENTS)
+    OptimizerWithGradientClip = _generate_optimizer_class_with_gradient_clipping(
+        optimizer_type, per_param_clipper=grad_clipper
+    )
+    if isinstance(optimizer, flow.optim.Optimizer):
+        optimizer.__class__ = OptimizerWithGradientClip  # a bit hacky, not recommended
+        return optimizer
+    else:
+        return OptimizerWithGradientClip
