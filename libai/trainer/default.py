6388e258f0b1ad56ab31411af1a384bc16c78121
rentainhe <596106517@qq.com>
2022-01-07 10:39:45 +0800

    complete graph debug info

--- libai/trainer/default.py
+++ libai/trainer/default.py
@@ -370,11 +370,11 @@ class DefaultTrainer(TrainerBase):
             try_get_key(cfg, "graph") is not None
         ), "cfg must contain `graph` namespace"
         graph = build_graph(cfg.graph, model, optimizer, lr_scheduler, is_train)
-        # logger = logging.getLogger(__name__)
-        # graph_debug = try_get_key(cfg, "graph", default=0)
-        # if graph_debug:
-        #     logger.info("")
-        graph.debug(cfg.graph.debug)
+        logger = logging.getLogger(__name__)
+        debug_graph = try_get_key(cfg, "debug", default=0)
+        if debug_graph >= 0:
+            logger.info("Graph debug mode on, automatically output debug info.")
+            graph.debug(cfg.graph.debug)
         return graph
 
     @classmethod
