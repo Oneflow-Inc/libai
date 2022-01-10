31025237d21c4352e480e021a345bbc99d576142
dangkai4u <dangkai4u@outlook.com>
2022-01-10 17:27:22 +0800

    change init file

--- libai/data/__init__.py
+++ libai/data/__init__.py
@@ -13,44 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .bert_dataset import BertDataset
-from .gpt_dataset import GPT2Dataset
-from .data_utils.data_utils import get_indexed_dataset, get_prefixes_and_weights
-from .data_utils.split_dataset import split_ds
-from .data_utils.blendable_dataset import BlendableDataset
-
-def build_single_dataset(args, tokenizer, data_prefix, data_impl, split=None, skip_warmup=False, data_type='gpt'):
-    indexed_dataset = get_indexed_dataset(data_prefix, data_impl, skip_warmup=skip_warmup)
-    if data_type == 'bert':
-        total_dataset = BertDataset(tokenizer, data_prefix, indexed_dataset, max_seq_length=args.max_seq_length, 
-                                    mask_lm_prob=args.mask_lm_prob, binary_head=args.binary_head)
-    else:
-        total_dataset = GPT2Dataset(tokenizer, data_prefix, indexed_dataset, max_seq_length=args.max_seq_length)
-    train_dataset, valid_dataset, test_dataset = split_ds(total_dataset, split)
-    return train_dataset, valid_dataset, test_dataset
-
-def build_dataset(args, tokenizer, data_prefix, data_impl, split=None, skip_warmup=False, data_type='gpt'):
-    if len(data_prefix) == 1:
-        return build_single_dataset(args, tokenizer, data_prefix[0], data_impl, split, skip_warmup, data_type)
-    
-    prefixes, weights = get_prefixes_and_weights(data_prefix)
-    train_datasets, valid_datasets, test_datasets = [], [], []
-    for prefix in range(prefixes):
-        train_dataset, valid_dataset, test_dataset = build_single_dataset(args, tokenizer, prefix, data_impl, split, skip_warmup, data_type)
-        if train_dataset:
-            train_datasets.append(train_dataset)
-        if valid_dataset:
-            valid_datasets.append(valid_dataset)
-        if test_dataset:
-            test_datasets.append(test_dataset)
-    
-    blending_train_dataset, blending_valid_dataset, blending_test_dataset = None, None, None
-    if len(train_datasets) > 0:
-        blending_train_dataset = BlendableDataset(train_datasets, weights)
-    if len(valid_datasets) > 0:
-        blending_valid_dataset = BlendableDataset(valid_datasets, weights)
-    if len(test_datasets) > 0:
-        blending_test_dataset = BlendableDataset(test_datasets, weights)
-    
-    return blending_train_dataset, blending_valid_dataset, blending_test_dataset 
-
+from .build import DATASET_REGISTRY, build_dataset
