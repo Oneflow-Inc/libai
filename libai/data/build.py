4991df1975e8d8c7b4c5431f91edaa980405e2ef
rentainhe <596106517@qq.com>
2022-01-12 20:43:48 +0800

    update test loader build func

--- libai/data/build.py
+++ libai/data/build.py
@@ -44,6 +44,9 @@ def build_image_train_loader(dataset, batch_size, sampler=None, num_workers=4, c
 
 
 def build_image_test_loader(dataset, batch_size, sampler=None, num_workers=4, collate_fn=None, **kwargs):
+
+    collate_fn = trivial_batch_collator if collate_fn is None else collate_fn
+
     return data.DataLoader(dataset, batch_size=batch_size, sampler=sampler, num_workers=num_workers, collate_fn=collate_fn, **kwargs)
 
 
