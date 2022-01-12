f534968b4ec258540d4c39e5b72ccd7e188219d4
rentainhe <596106517@qq.com>
2022-01-12 20:51:07 +0800

    refine unittest

--- tests/data/test_cv_loader.py
+++ tests/data/test_cv_loader.py
@@ -15,11 +15,19 @@
 
 from libai.config.instantiate import instantiate
 from libai.config import LazyConfig
+from libai.data.structures import Instance
 
 cfg = LazyConfig.load("./configs/common/data/cv_data.py")
 
-train_loader = instantiate(cfg.dataloader.train)
+train_loader, val_loader, test_loader = instantiate(cfg.dataloader.train)
 assert len(train_loader) == 80073
+for sample in train_loader:
+    assert isinstance(sample, Instance)
+    break
 
 test_loader = instantiate(cfg.dataloader.test)
 assert len(test_loader[0]) == 3125
+for loader in test_loader:
+    for sample in loader:
+        assert isinstance(sample, Instance)
+        break
