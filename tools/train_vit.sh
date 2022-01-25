eb126175819a0ca512f26b71ddd351b7239a9804
rentainhe <596106517@qq.com>
2022-01-25 11:00:27 +0800

    add train engine

---
+++ tools/train_vit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CONFIG=configs/vit_imagenet.py #output/your_task/config.yaml
+GPUS=1
+NODE=1
+NODE_RANK=0
+PORT=2345
+
+python3 -m oneflow.distributed.launch \
+    --nproc_per_node $GPUS \
+    --nnodes $NODE \
+    --node_rank $NODE_RANK \
+    --master_addr $PORT \
+    tools/train_net.py \
+    --config-file $CONFIG \
