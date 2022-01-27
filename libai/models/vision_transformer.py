ba022a9e320a1cf14704ef9af62a24fd15f34e89
rentainhe <596106517@qq.com>
2022-01-27 16:25:09 +0800

    fix format bug

--- libai/models/vision_transformer.py
+++ libai/models/vision_transformer.py
@@ -28,28 +28,25 @@ from libai.config.config import configurable
 from .build import GRAPH_REGISTRY, MODEL_ARCH_REGISTRY
 from .utils import GraphBase
 
+
 def drop_path(x, drop_prob: float = 0.5, training: bool = False):
-    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
-    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
-    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
-    'survival rate' as the argument.
-    """
     if drop_prob == 0.0 or not training:
         return x
     keep_prob = 1 - drop_prob
-    shape = (x.shape[0],) + (1,) * (
-        x.ndim - 1
-    )  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = flow.rand(*shape, dtype=x.dtype, sbp=flow.sbp.broadcast, placement=x.placement).to_consistent(sbp=x.sbp) + keep_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = (
+        flow.rand(
+            *shape, dtype=x.dtype, sbp=flow.sbp.broadcast, placement=x.placement
+        ).to_consistent(sbp=x.sbp)
+        + keep_prob
+    )
     random_tensor = random_tensor.floor()  # binarize
     output = x.div(keep_prob) * random_tensor
     return output
 
+
 class DropPath(nn.Module):
-    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
-    """
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
 
     def __init__(self, drop_prob=None):
         super(DropPath, self).__init__()
@@ -57,6 +54,7 @@ class DropPath(nn.Module):
 
     def forward(self, x):
         return drop_path(x, self.drop_prob, self.training)
+
 
 class Attention(nn.Module):
     def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0.0, proj_drop=0.0):
