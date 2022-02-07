f984137f0da1f3490e4de9193ce20235847d3275
rentainhe <596106517@qq.com>
2022-02-07 09:48:17 +0000

    fix main vit cfg

--- configs/vit_imagenet.py
+++ configs/vit_imagenet.py
@@ -2,14 +2,29 @@ from libai.config import LazyCall
 from .common.models.vit import vit_model as model
 from .common.train import train
 from .common.optim import optim
-from .common.data.cifar import dataloader
+from .common.data.imagenet import dataloader
 
 from libai.models import VisionTransformerGraph
+from flowvision.data import Mixup
 from flowvision.loss.cross_entropy import SoftTargetCrossEntropy
 
-model.num_classes = 100
-model.loss_func = LazyCall(SoftTargetCrossEntropy)()
+# Refine data path to imagenet
+dataloader.train.dataset[0].root = "/path/to/imagenet"
+dataloader.test[0].dataset.root = "/path/to/imagenet"
 
+# Add MixupFunc
+dataloader.train.mixup_func = LazyCall(Mixup)(
+    mixup_alpha=0.8,
+    cutmix_alpha=1.0,
+    prob=1.0,
+    switch_prob=0.5,
+    mode="batch",
+    num_classes=1000,
+)
+
+# Refine model cfg for vit training on imagenet
+model.num_classes = 1000
+model.loss_func = LazyCall(SoftTargetCrossEntropy)()
 
 # Refine optimizer cfg for vit model
 optim.lr = 5e-4
@@ -17,7 +32,7 @@ optim.eps = 1e-8
 optim.weight_decay = 0.05
 
 # Refine train cfg for vit model
-train.train_micro_batch_size = 128
+train.train_micro_batch_size = 64
 train.test_micro_batch_size = 128
 train.train_epoch = 300
 train.warmup_ratio = 20 / 300
