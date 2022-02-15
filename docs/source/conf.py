542379e29f40a5b48fa483060cde33b0a8bc241f
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2022-02-15 16:18:25 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 
 def setup(app):
