f134a76bdd8504f4748e44a048e5aa5f4a44c4e9
l1aoxingyu <sherlockliao01@gmail.com>
2022-02-21 18:50:10 +0800

    add bert train to unittest
    Summay:
    1. Support file download and md5 check;
    2. Support Eager/Graph train with 100 iters

--- tests/models/test_bert.py
+++ tests/models/test_bert.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+import os
 import tempfile
 import unittest
 
@@ -20,24 +22,34 @@ from libai.config import LazyConfig
 from libai.trainer import DefaultTrainer, hooks
 from libai.trainer.default import _check_batch_size
 from libai.utils import distributed as dist
+from libai.utils.file_utils import get_data_from_cache
+from libai.utils.logger import setup_logger
+
+VOCAB_URL = "https://oneflow-static.oss-cn-beijing.aliyuncs.com/ci-files/dataset/libai/bert_dataset/bert-base-chinese-vocab.txt"  # noqa
+BIN_DATA_URL = "https://oneflow-static.oss-cn-beijing.aliyuncs.com/ci-files/dataset/libai/bert_dataset/loss_compara_content_sentence.bin"  # noqa
+IDX_DATA_URL = "https://oneflow-static.oss-cn-beijing.aliyuncs.com/ci-files/dataset/libai/bert_dataset/loss_compara_content_sentence.idx"  # noqa
+
+VOCAB_MD5 = "3b5b76c4aef48ecf8cb3abaafe960f09"
+BIN_DATA_MD5 = "b842467bd5ea7e52f7a612ea6b4faecc"
+IDX_DATA_MD5 = "cf5963b8543f0a7a867361eb980f0372"
 
 
 class TestBertModel(unittest.TestCase):
     def setUp(self) -> None:
+        cache_dir = os.path.join(os.getenv("ONEFLOW_TEST_CACHE_DIR", "./data_test"), "bert_data")
+
         cfg = LazyConfig.load("configs/bert_large_pretrain.py")
 
-        # set data path
-        # prepare dataset
+        # prepare data path
+        vocab_path = get_data_from_cache(VOCAB_URL, cache_dir, md5=VOCAB_MD5)
+        data_prefix_path = get_data_from_cache(BIN_DATA_URL, cache_dir, md5=BIN_DATA_MD5)
+        get_data_from_cache(IDX_DATA_URL, cache_dir, md5=IDX_DATA_MD5)
+        data_prefix = data_prefix_path[:-4]
 
-        cfg.tokenization.tokenizer.vocab_file = (
-            "/workspace/dataset/bert_data/bert-base-chinese-vocab.txt"
-        )
-        cfg.dataloader.train.dataset[
-            0
-        ].data_prefix = "/workspace/dataset/bert_data/loss_compara_content_sentence"
-        cfg.dataloader.train.dataset[
-            0
-        ].indexed_dataset.data_prefix = "/workspace/dataset/bert_data/loss_compara_content_sentence"
+        # set tokenizer and data config
+        cfg.tokenization.tokenizer.vocab_file = vocab_path
+        cfg.dataloader.train.dataset[0].data_prefix = data_prefix
+        cfg.dataloader.train.dataset[0].indexed_dataset.data_prefix = data_prefix
 
         # set training config
         cfg.train.train_epoch = 0
@@ -48,7 +60,7 @@ class TestBertModel(unittest.TestCase):
         cfg.train.output_dir = tempfile.mkdtemp()
 
         # set distributed config
-        cfg.train.dist.data_parallel_size = 1
+        cfg.train.dist.data_parallel_size = 2
         cfg.train.dist.tensor_parallel_size = 1
         cfg.train.dist.pipeline_parallel_size = 1
         cfg.train.dist.pipeline_num_layers = 100
@@ -58,6 +70,9 @@ class TestBertModel(unittest.TestCase):
         cfg.model.cfg.hidden_size = 384
         cfg.model.cfg.hidden_layers = 4
         cfg.train.recompute_grad.enabled = True
+
+        rank = dist.get_rank()
+        setup_logger(cfg.train.output_dir, distributed_rank=rank)
 
         dist.setup_dist_util(cfg.train.dist)
         _check_batch_size(cfg)
