926921f98b323b1b2321ff073c4571e05a79ac8c
l1aoxingyu <sherlockliao01@gmail.com>
2022-02-24 14:44:11 +0800

    fix bug for data download and pipeline parallelism
    Summary:
    1. Make data downloading in main process and others waiting;
    2. Get metric on main process from others, because in pipeline parallelism, result and metric will be computed in last stage. Then the main process result will be empty.

--- dev/model_test.sh
+++ dev/model_test.sh
@@ -3,4 +3,5 @@
 # cd to libai project root
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-ONEFLOW_TEST_DEVICE_NUM=4 python3 -m oneflow.distributed.launch --nproc_per_node 4 -m unittest discover -f -v -s ./tests/models
+export ONEFLOW_TEST_DEVICE_NUM=4
+python3 -m oneflow.distributed.launch --nproc_per_node 4 -m unittest discover -f -v -s ./tests/models
