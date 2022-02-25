c74ac5703d5ed3fd2a845e7dd619e7d6d0d1ebd4
l1aoxingyu <sherlockliao01@gmail.com>
2022-02-25 11:15:59 +0800

    add t5 unittest

--- configs/t5_pp_pretrain.py
+++ configs/t5_pp_pretrain.py
@@ -17,8 +17,8 @@ model.cfg.num_attention_heads = 12
 model.cfg.hidden_size = 384
 model.cfg.hidden_layers = 6
 
-train.dist.tensor_parallel_size = 1
-train.dist.pipeline_parallel_size = 2
+train.dist.tensor_parallel_size = 2
+train.dist.pipeline_parallel_size = 1
 # encoder_layers + decoder_layers
 train.dist.pipeline_num_layers = 2 * model.cfg.hidden_layers
 
