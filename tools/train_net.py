5d4d74ba729b2c638626a0d0b09b7e99a8fd73eb
CPFLAME <410070869@qq.com>
2022-03-10 19:41:37 +0800

    del import cv

--- tools/train_net.py
+++ tools/train_net.py
@@ -17,8 +17,6 @@ import logging
 import os
 import sys
 
-from cv2 import log
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from libai.config import LazyConfig, default_argument_parser, try_get_key
 from libai.engine import DefaultTrainer, default_setup
