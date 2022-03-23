14eca751408f1f857ec34c3cd411a364433622f6
59rentainhe <596106517@qq.com>
2022-03-23 03:56:44 +0000

    refine setup and cn docs

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from typing import List
 
 from setuptools import Extension, find_packages, setup
 
-version = "0.1.1"
+version = "0.1.0"
 package_name = "LiBai"
 cwd = os.path.dirname(os.path.abspath(__file__))
 
