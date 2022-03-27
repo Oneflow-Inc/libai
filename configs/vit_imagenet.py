1d6d27b4ab041333b8fdbe7a9f48083410267c19
59rentainhe <596106517@qq.com>
2022-03-27 02:18:48 +0000

    align weight decay

--- configs/vit_imagenet.py
+++ configs/vit_imagenet.py
@@ -30,6 +30,9 @@ dataloader.train.mixup_func = LazyCall(Mixup)(
 optim.lr = 1e-3  # 5e-4 * 1024 (batchsize) / 512
 optim.eps = 1e-8
 optim.weight_decay = 0.05
+optim.params.clip_grad_max_norm = None
+optim.params.clip_grad_norm_type = None
+optim.params.overrides = {"pos_embed": {"weight_decay": 0.}, "cls_token": {"weight_decay": 0.}}
 
 # Refine train cfg for vit model
 train.train_micro_batch_size = 128
