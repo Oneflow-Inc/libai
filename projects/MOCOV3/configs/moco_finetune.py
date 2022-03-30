a45e789b69417d462e6306bee3f307082d633ad5
ZiqiuChi <chiziqiu0923@gmail.com>
2022-03-30 08:15:21 +0000

    refine configs

--- projects/MOCOV3/configs/moco_finetune.py
+++ projects/MOCOV3/configs/moco_finetune.py
@@ -15,18 +15,6 @@ model.weight_style = "pytorch"
 dataloader.train.dataset[0].root = "/dataset/extract"
 dataloader.test[0].dataset.root = "/dataset/extract"
 
-# Refine optimizer cfg for moco v3 model
-# Reference:
-# https://github.com/facebookresearch/moco-v3/blob/main/CONFIG.md
-# https://github.com/facebookresearch/moco-v3/blob/main/main_lincls.py
-
-base_lr = 3.
-actual_lr = base_lr * (train.train_micro_batch_size * 8 / 256)
-
-optim.lr = actual_lr
-optim.weight_decay = 0.
-optim.momentum = .9
-
 # Refine train cfg for moco v3 model
 train.train_micro_batch_size = 128
 train.test_micro_batch_size = 32
@@ -34,6 +22,16 @@ train.train_epoch = 100
 train.warmup_ratio = 5 / 100
 train.log_period = 1
 train.evaluation.eval_period = 1000
+
+# Refine optimizer cfg for moco v3 model
+# Reference:
+# https://github.com/facebookresearch/moco-v3/blob/main/CONFIG.md
+# https://github.com/facebookresearch/moco-v3/blob/main/main_lincls.py
+base_lr = 3.
+actual_lr = base_lr * (train.train_micro_batch_size * 8 / 256)
+optim.lr = actual_lr
+optim.weight_decay = 0.
+optim.momentum = .9
 
 # Scheduler
 train.scheduler.warmup_factor = 0.001
