9f1e21e852111568eca7ee9defccb0ec0ab66300
rentainhe <596106517@qq.com>
2022-03-31 13:25:24 +0000

    update

--- tools/train.sh
+++ tools/train.sh
@@ -6,7 +6,7 @@ GPUS=$3
 NODE=${NODE:-1}
 NODE_RANK=${NODE_RANK:-0}
 ADDR=${ADDR:-127.0.0.1}
-PORT=${PORT:-23456}
+PORT=${PORT:-12345}
 
 python3 -m oneflow.distributed.launch \
 --nproc_per_node $GPUS --nnodes $NODE --node_rank $NODE_RANK --master_addr $ADDR --master_port $PORT \
