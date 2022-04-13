266402ea0f35a85b3e27c70359d137a9c3f3f2f8
lxy <sherlockliao01@gmail.com>
2022-04-13 12:22:35 +0800

    remove useless reference

--- libai/config/arguments.py
+++ libai/config/arguments.py
@@ -13,11 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# --------------------------------------------------------
-# References:
-# https://github.com/facebookresearch/detectron2/blob/main/detectron2/config/lazy.py
-# --------------------------------------------------------
-
 
 import argparse
 import sys
