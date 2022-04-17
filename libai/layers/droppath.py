f6dfe0bfc60177a70f719f0640d8ea48af4d16bd
Ldpe2G <liangdepeng@gmail.com>
2022-04-17 12:02:06 +0000

    fix 2d sbp eager global train crash

--- libai/layers/droppath.py
+++ libai/layers/droppath.py
@@ -17,14 +17,14 @@ import oneflow as flow
 import oneflow.nn as nn
 
 
-def drop_path(x, drop_prob: float = 0.5, training: bool = False):
+def drop_path(x, drop_prob: float = 0.5, training: bool = False, generator: flow.Generator = None):
     """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
 
     if drop_prob == 0.0 or not training:
         return x
     keep_prob = 1 - drop_prob
     shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = flow.rand(*shape, dtype=x.dtype, sbp=x.sbp, placement=x.placement) + keep_prob
+    random_tensor = flow.rand(*shape, dtype=x.dtype, generator=generator, sbp=x.sbp, placement=x.placement) + keep_prob
     random_tensor = random_tensor.floor()  # binarize
     output = x.div(keep_prob) * random_tensor
     return output
@@ -33,9 +33,11 @@ def drop_path(x, drop_prob: float = 0.5, training: bool = False):
 class DropPath(nn.Module):
     """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
 
-    def __init__(self, drop_prob=None):
+    def __init__(self, drop_prob=None, seed=0):
         super(DropPath, self).__init__()
         self.drop_prob = drop_prob
+        self.generator = flow.Generator()
+        self.generator.manual_seed(seed)
 
     def forward(self, x):
-        return drop_path(x, self.drop_prob, self.training)
+        return drop_path(x, self.drop_prob, self.training, self.generator)
