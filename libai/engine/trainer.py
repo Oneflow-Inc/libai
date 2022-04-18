dd53f11bccaec3bb9c272efce37e5a82a4fa12d0
Ren Tianhe <48727989+rentainhe@users.noreply.github.com>
2022-04-18 15:22:07 +0800

    Support Tensorboard Writer (#271)
    * add tensorboard writer and update readthedocs
    
    * refine trainer
    
    * refine format
    
    * update requirements

--- libai/engine/trainer.py
+++ libai/engine/trainer.py
@@ -165,13 +165,12 @@ class TrainerBase:
             h.after_train()
 
     def before_step(self):
+        self.storage.iter = self.iter
         for h in self._hooks:
             h.before_step()
 
     def after_step(self):
-        self.storage.iter = self.iter + 1
         self.storage.samples = (self.iter + 1) * self.cfg.train.global_batch_size
-
         for h in self._hooks:
             h.after_step()
 
