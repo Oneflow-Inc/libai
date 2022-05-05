8823563fd4ac63e66edc79827e3b5bf998e3f5d8
lxy <sherlockliao01@gmail.com>
2022-05-05 13:35:12 +0800

    change for oneflow zero test

--- configs/bert_3.9B_pp_pretrain.py
+++ configs/bert_3.9B_pp_pretrain.py
@@ -7,11 +7,11 @@ from .common.data.bert_dataset import dataloader, tokenization
 
 from .common.models.graph import graph
 
-# vocab_file = "./data_test/bert_data/bert-base-chinese-vocab.txt"
-# data_prefix = "./data_test/bert_data/loss_compara_content_sentence"
+vocab_file = "./data_test/bert_data/bert-base-chinese-vocab.txt"
+data_prefix = "./data_test/bert_data/loss_compara_content_sentence"
 
-vocab_file = '/cognitive_comp/ganruyi/Megatron/vocab/bert-base-chinese-vocab.txt'
-data_prefix= '/cognitive_comp/ganruyi/experiments/oneflow_bert_3.9B/wudao180G_bert_text_sentence'
+# vocab_file = '/cognitive_comp/ganruyi/Megatron/vocab/bert-base-chinese-vocab.txt'
+# data_prefix= '/cognitive_comp/ganruyi/experiments/oneflow_bert_3.9B/wudao180G_bert_text_sentence'
 
 
 tokenization.tokenizer.vocab_file = vocab_file
@@ -21,7 +21,7 @@ dataloader.train.dataset[0].indexed_dataset.data_prefix = data_prefix
 # Bert-large model config
 model.cfg.num_attention_heads = 40
 model.cfg.hidden_size = 2560
-model.cfg.hidden_layers = 48
+model.cfg.hidden_layers = 16
 
 train.train_micro_batch_size = 16
 train.dist.pipeline_parallel_size = 4
@@ -38,4 +38,4 @@ train.train_iter = 200
 train.evaluation.evaluator = LazyCall(PPLEvaluator)()
 train.evaluation.eval_iter = 1000
 
-train.output_dir = "output/bert_3.9B_output"
+train.output_dir = "output/bert_3.9B_output_0.7of"
