7563376ddf3a8b47b88547545c88df5a13d31c49
hihippie <chiziiqiu0923@gmail.com>
2022-05-26 10:56:50 +0800

    add pre_normalized transformer_layer logic

--- projects/DETR/modeling/transformer_layer.py
+++ projects/DETR/modeling/transformer_layer.py
@@ -19,7 +19,7 @@ class DetrTransformerLayer(TransformerLayer):
         num_attention_heads, 
         is_decoder=False,
         dropout_prob=0.0,
-        apply_residual_post_layernorm=False
+        normalize_before=False
     ):
         
         super(DetrTransformerLayer, self).__init__(
@@ -27,14 +27,9 @@ class DetrTransformerLayer(TransformerLayer):
             ffn_hidden_size, 
             num_attention_heads, 
             is_decoder=is_decoder,
-            apply_residual_post_layernorm=apply_residual_post_layernorm
         )    
 
-        self.hidden_size = hidden_size
-        self.ffn_hidden_size = ffn_hidden_size
-        self.num_attention_heads = num_attention_heads
-        self.is_decoder = is_decoder
-        self.apply_residual_post_layernorm = apply_residual_post_layernorm
+        self.normalize_before = normalize_before
         
         self.dropout1 = nn.Dropout(dropout_prob)
         self.dropout2 = nn.Dropout(dropout_prob)
@@ -52,16 +47,12 @@ class DetrTransformerLayer(TransformerLayer):
                 attention_dropout_prob=dropout_prob
             )
             self.dropout3 = nn.Dropout(dropout_prob)
-            # self.post_cross_attention_layernorm = nn.LayerNorm(self.hidden_size)
             
         self.mlp = MLPLayer(
             self.hidden_size,
             self.ffn_hidden_size,
         )
             
-        # self.input_layernorm = nn.LayerNorm(self.hidden_size)
-        # self.post_attention_layernorm = nn.LayerNorm(self.hidden_size)
-    
     def forward(
         self,
         hidden_states,
@@ -80,16 +71,6 @@ class DetrTransformerLayer(TransformerLayer):
             attention_mask: the combination of key padding mask and casual mask of hidden states
                 with shape (batch_size, 1, seq_length, seq_length) and the sbp
                 signature is (S(0), B),
-            encoder_states: encoder output with shape (batch_size, seq_length, hidden_size)
-                and the sbp signature is (S(0), B), which will be used in cross attention.
-            encoder_attention_mask: key padding mask of encoder states with shape
-                (batch_size, 1, seq_length, seq_length) and the sbp signature is (S(0), B).
-            past_key_value: tuple of key and value, each shape is
-                (seq_length, bsz, num_heads, head_size), For decoder layer,
-                the past_key_value contains the states both from self attention
-                and cross attention.
-            use_cache: it will be set to `True` when the model is in the inference phase and
-                used for incremental decoding.
         """
         # # Change placement for pipeline parallelsim
         # hidden_states = hidden_states.to_global(placement=dist.get_layer_placement(self.layer_idx))
@@ -104,10 +85,19 @@ class DetrTransformerLayer(TransformerLayer):
         #     key_padding_mask = key_padding_mask.to_global(
         #         placement=dist.get_layer_placement(self.layer_idx)
         #     )
-        if self.is_decoder:
-            query = key = self.with_pos_embed(hidden_states, query_position_embedding)
+        
+        if self.normalize_before:
+            layernorm_hidden_states = self.input_layernorm(hidden_states)
+            if self.is_decoder:
+                query = key = self.with_pos_embed(layernorm_hidden_states, query_position_embedding)
+            else:
+                query = key = self.with_pos_embed(layernorm_hidden_states, position_embedding)
         else:
-            query = key = self.with_pos_embed(hidden_states, position_embedding)
+            if self.is_decoder:
+                query = key = self.with_pos_embed(hidden_states, query_position_embedding)
+            else:
+                query = key = self.with_pos_embed(hidden_states, position_embedding)
+                            
         attention_output = self.self_attention(
             (query, key, hidden_states),
             attention_mask=attention_mask,
@@ -115,13 +105,12 @@ class DetrTransformerLayer(TransformerLayer):
         )
         attention_output = self.dropout1(attention_output)
         
-        if self.apply_residual_post_layernorm:
-            residual = layernorm_output
-        else:
-            residual = hidden_states
-        hidden_states = residual + attention_output
+        hidden_states = hidden_states + attention_output
 
-        layernorm_output = self.input_layernorm(hidden_states)
+        if self.normalize_before:
+            layernorm_output = self.post_attention_layernorm(hidden_states)
+        else:
+            layernorm_output = self.input_layernorm(hidden_states)
         
         if self.is_decoder:
             attention_output = self.cross_attention(
@@ -132,11 +121,7 @@ class DetrTransformerLayer(TransformerLayer):
                 key_padding_mask=memory_key_padding_mask
             )
             attention_output = self.dropout3(attention_output)
-            if self.apply_residual_post_layernorm:
-                residual = hidden_states
-            else:
-                residual = layernorm_output
-            hidden_states = residual + attention_output
+            hidden_states = layernorm_output + attention_output
             layernorm_output = self.post_cross_attention_layernorm(hidden_states)
 
         mlp_output = self.mlp(layernorm_output)
@@ -149,14 +134,14 @@ class DetrTransformerLayer(TransformerLayer):
 
         output = residual + mlp_output
         
-        output = self.post_attention_layernorm(output)
-
+        if not self.normalize_before:
+            output = self.post_attention_layernorm(output)
+            
         return output
     
     def with_pos_embed(self, tensor, pos):
         
         return tensor if pos is None else tensor + pos
-    
     
     
 class MLPLayer(MLP):
