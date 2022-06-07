84d7658d0427a0f4e8b075c6079c9141b7d8b17f
Zipeng Xie <53039617+xiezipeng-ML@users.noreply.github.com>
2022-06-07 15:26:09 +0800

    Inference for classification (#285)
    * add bertforclassification
    
    * add test_text_classification.py
    
    * add configs/bert_classification.py
    
    * delete useless import
    
    * refine
    
    * refine
    
    * refine
    
    * add BertForClassification to init.py

--- libai/models/__init__.py
+++ libai/models/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .bert_model import BertForPreTraining, BertModel
+from .bert_model import BertForPreTraining, BertModel, BertForClassification
 from .build import build_graph, build_model
 from .t5_model import T5ForPreTraining, T5Model
 from .gpt_model import GPTForPreTraining, GPTModel
@@ -26,6 +26,7 @@ __all__ = [
     "build_graph",
     "BertModel",
     "BertForPreTraining",
+    "BertForClassification",
     "T5Model",
     "T5ForPreTraining",
     "GPTModel",
