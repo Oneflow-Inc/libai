fd43ddf78aaa32f009053964c46a4399f5c8a74e
wyg1997 <wangyinggang@foxmail.com>
2022-06-07 15:42:43 +0800

    add comment for auto_parallel conf

--- configs/common/models/graph.py
+++ configs/common/models/graph.py
@@ -8,9 +8,9 @@ graph = dict(
     debug=-1,  # debug mode for graph
     auto_parallel=dict(
         enabled=False,
-        prune_parallel_cast_ops=False,
-        mainstem_algo=True,
-        sbp_collector=False,
+        prune_parallel_cast_ops=False,  # ignore all .to_global() operator in graph
+        mainstem_algo=True,  # consider overlapping calculate time and transfer time
+        sbp_collector=False,  # use proxy node when one node transfer to many nodes
     ),
     train_graph=LazyCall(GraphBase)(
         is_train=True,
