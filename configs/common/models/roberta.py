324302eb076c87733818ef0b32c6ca332a7c5a03
xiezipeng <xzpaiks@163.com>
2022-06-23 03:04:21 +0000

    update robberta config

--- configs/common/models/roberta.py
+++ configs/common/models/roberta.py
@@ -19,7 +19,7 @@ cfg = dict(
     bias_gelu_fusion=False,
     bias_dropout_fusion=False,
     scale_mask_softmax_fusion=True,
-    apply_query_key_layer_scaling=False,
+    apply_query_key_layer_scaling=True,
     apply_residual_post_layernorm=True,
     amp_enabled=False,
 )
