50d8560d78a10249ad53ec9896fb124176de01e0
zhanggj821 <zgj18811414604@163.com>
2022-07-26 15:22:39 +0800

    add load pretrained swin modle

---
+++ libai/models/utils/model_utils/swin_utils.py
@@ -0,0 +1,215 @@
+import json
+
+import oneflow as flow
+
+from libai.models import SwinTransformer
+
+from .base_utils import LoadPretrainedBase
+
+
+class LoadPretrainedSwin(LoadPretrainedBase):
+    def __init__(self, model, default_cfg, pretrained_model_path, **kwargs):
+        super().__init__(model, default_cfg, pretrained_model_path, **kwargs)
+        
+        """NOTE: base_model_prefix_1 is SWIN's prefix in Transformers.
+        base_model_prefix_2 is SWIN's prefix in LiBai."""
+    
+        self.base_model_prefix_1 = "swin"
+        self.base_model_prefix_2 = "swin"
+    
+    def _convert_state_dict(self, flow_state_dict, cfg):
+        """Convert state_dict's keys to match model.
+
+        Args:
+            flow_state_dict (OrderedDict): model state dict.
+            cfg (dict): model's default config dict.
+
+        Returns:
+            OrderedDict: flow state dict.
+        """
+        # The converted checkpoint.
+        oneflow_state_dict = flow_state_dict.copy()
+
+        # Get configs
+        num_heads = cfg.get("num_heads")
+        embed_dim = [cfg.get("embed_dim") * 2**i for i in range(0, 4)]
+        
+        head_size = [dim/head for dim, head in  (embed_dim, num_heads)]
+
+        # prefix
+        has_prefix = any(s.startswith(self.base_model_prefix_1) for s in oneflow_state_dict)
+
+        prefix = "swin." if has_prefix else ""
+        index_idx_1 = 2
+        index_idx_2 = 4
+        # qkv_idx = 7
+        
+        old_keys = oneflow_state_dict.keys()
+
+        for key in list(old_keys):      # 缺少mask
+
+            # Convert swin's embedding layers
+            if key.startwith("embeddings"):
+                if "patch_embeddings.projection" in key:
+                    new_key = key.replace("patch_embeddings.projection", "patch_embed.proj").replace("embeddings.", "")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "norm.weight" in key:
+                    new_key = key.replace("embeddings", "patch_embed")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "norm.bias" in key:
+                    new_key = key.replace("embeddings", "patch_embed")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                else:
+                    oneflow_state_dict[key] = oneflow_state_dict[key]
+
+            # Convert swin's encoder layers
+            elif key.startwith("encoder"):
+                if "layernorm_before" in key:
+                    new_key = prefix + key.replace("layernorm_before", "norm1").replace("encoder.", "")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "layernorm_after" in key:
+                    new_key = prefix + key.replace("layernorm_after", "norm2").replace("encoder.", "")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                    
+                # if "attention" in key:
+                elif "attention" in key:
+                    # if "self" in key:
+                    if "self" in key:
+                        if "relative_position_bias_table" in key:  # convert relative_position_bias_table
+                            new_key = prefix + key.replace("attention.self", "attn").replace("encoder.", "")
+                            oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                        else:   # conver qkv
+                            index_layer = key.split(".")[index_idx_1]
+                            index_block = key.split(".")[index_idx_2]
+                            q_w = key
+                            k_w = q_w.replace("query", "key")
+                            v_w = q_w.replace("query", "value")
+                            q_b = q_w.replace("weight", "bias")
+                            k_b = k_w.replace("weight", "bias")
+                            v_b = v_w.replace("weight", "bias")
+
+                            qkv_w = flow.cat(
+                                (
+                                    oneflow_state_dict.pop(q_w),
+                                    oneflow_state_dict.pop(k_w),
+                                    oneflow_state_dict.pop(v_w),
+                                ),
+                                dim=0,
+                            )
+                            qkv_b = flow.cat(
+                                (
+                                    oneflow_state_dict.pop(q_b),
+                                    oneflow_state_dict.pop(k_b),
+                                    oneflow_state_dict.pop(v_b),
+                                ),
+                                dim=-1,
+                            )
+
+                            qkv_w = self._fix_qkv_ordering(qkv_w, head_size[index_layer], num_heads[index_layer])
+                            qkv_b = self._fix_qkv_ordering(qkv_b, head_size[index_layer], num_heads[index_layer])
+
+                            new_key = (
+                                prefix + "layers."  + index_layer + ".blocks." + index_block + ".attn.qkv.weight"
+                            )
+                            oneflow_state_dict[new_key] = qkv_w
+                            
+                            new_key = new_key.replace("weight", "bias")
+                            oneflow_state_dict[new_key] = qkv_b
+                    elif "output" in key:
+                        index_1 = key.split(".")[index_idx_1]
+                        index_2 = key.split(".")[index_idx_2]
+                        if "dense" in key:
+                            if "weight" in key:
+                                new_key = prefix + "layers." + index_1 + ".blocks." + index_2 + ".attn.proj.weight"
+                                oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                            elif "bias" in key:
+                                new_key = prefix + "layers." + index_1 + ".blocks." + index_2 + ".attn.proj.bias"
+                                oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                       
+                # Convert swin's intermediate layers
+                elif "intermediate" in key:
+                    index_layer = key.split(".")[index_idx_1]
+                    index_block = key.split(".")[index_idx_2]
+                    if "weight" in key:
+                        w = key
+                        b = key.replace("weight", "bias")
+                        new_key = prefix + "layers." + index_layer + ".blocks." + index_block + ".mlp.dense_h_to_4h.weight"
+                        oneflow_state_dict[new_key] = oneflow_state_dict.pop(w)
+                        new_key = new_key.replace("weight", "bias")
+                        oneflow_state_dict[new_key] = oneflow_state_dict.pop(b)
+
+                # Convert swin's output layers
+                elif "output" in key:
+                    index_layer = key.split(".")[index_idx_1]
+                    index_block = key.split(".")[index_idx_2]
+                    if "dense.weight" in key:
+                        w = key
+                        b = w.replace("weight", "bias")
+                        new_key = prefix + prefix + "layers." + index_layer + ".blocks." + index_block + ".mlp.dense_4h_to_h.weight"
+                        oneflow_state_dict[new_key] = oneflow_state_dict.pop(w)
+                        new_key = new_key.replace("weight", "bias")
+                        oneflow_state_dict[new_key] = oneflow_state_dict.pop(b)
+                        
+                # Convert swin's downsample layers
+                elif "downsample" in key:
+                    new_key = prefix + key.replace("encoder.", "")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                    
+            elif key.startwith("layernorm"):
+                new_key = key.replace("layernorm", "norm")
+                oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+            
+            # Convert cls_head layers   # swin maybe some different
+            elif "cls" in key:  
+                if "predictions.bias" in key:
+                    new_key = "cls_head.lm_logits.bias"
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "dense.weight" in key:
+                    new_key = "cls_head.predictions.dense.weight"
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "dense.bias" in key:
+                    new_key = "cls_head.predictions.dense.bias"
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "LayerNorm.weight" in key:
+                    new_key = "cls_head.predictions.layernorm.weight"
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "LayerNorm.bias" in key:
+                    new_key = "cls_head.predictions.layernorm.bias"
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+                elif "seq_relationship" in key:
+                    new_key = key.replace("cls", "cls_head")
+                    oneflow_state_dict[new_key] = oneflow_state_dict.pop(key)
+            else:
+                oneflow_state_dict[key] = oneflow_state_dict.pop(key)
+                
+        return oneflow_state_dict
+    
+    
+    def _load_config_from_json(self, config_file):
+        """load config from `config.json`, and update default config.
+
+        Args:
+            config_file (str): Path of config file.
+        """
+        with open(config_file, mode="r", encoding="utf-8") as f:
+            cfg_dict = json.load(f)
+
+        # update default_cfg by config.json
+        self.default_cfg.img_size = cfg_dict["cfg_dict"]
+        self.default_cfg.patch_size = cfg_dict["patch_size"]
+        self.default_cfg.embed_dim = cfg_dict["embed_dim"]
+        self.default_cfg.depths = cfg_dict["depths"]
+        self.default_cfg.num_heads = cfg_dict["num_heads"]
+        self.default_cfg.window_size = cfg_dict["window_size"]
+        self.default_cfg.mlp_ratio = cfg_dict["mlp_ratio"]
+        self.default_cfg.qkv_bias = cfg_dict["qkv_bias"]
+        self.default_cfg.drop_path_rate = cfg_dict["drop_path_rate"]
+        
+        
+        # update default_cfg by kwargs
+        for k, v in self.kwargs.items():
+            self.default_cfg[k] = v
+
+if __name__ == "__main__":
+    model = SwinTransformer()
+    print(model)
