48e96a880898466eaa994b4686ac607cb9a2b015
xiezipeng <xzpaiks@163.com>
2022-08-02 04:07:40 +0000

    run linter.sh

--- projects/T5/datasets/dataset.py
+++ projects/T5/datasets/dataset.py
@@ -18,13 +18,15 @@ def get_data(path):
 
 def compute_input_and_target_lengths(inputs_length, noise_density, mean_noise_span_length):
     """This function is copy of `random_spans_helper <https://github.com/google-research/
-    text-to-text-transfer-transformer/blob/84f8bcc14b5f2c03de51bd3587609ba8f6bbd1cd/t5/data/preprocessors.py#L2466>`__ .
+    text-to-text-transfer-transformer/blob/84f8bcc14b5f2c03de51bd3587609ba8f6bbd1cd/
+    t5/data/preprocessors.py#L2466>`__ .
     Training parameters to avoid padding with random_spans_noise_mask.
     When training a model with random_spans_noise_mask, we would like to set the other
     training hyperparmeters in a way that avoids padding.
     This function helps us compute these hyperparameters.
-    We assume that each noise span in the input is replaced by extra_tokens_per_span_inputs sentinel tokens,
-    and each non-noise span in the targets is replaced by extra_tokens_per_span_targets sentinel tokens.
+    We assume that each noise span in the input is replaced by extra_tokens_per_span_inputs
+    sentinel tokens, and each non-noise span in the targets is replaced by
+    extra_tokens_per_span_targets sentinel tokens.
     This function tells us the required number of tokens in the raw example (for split_tokens())
     as well as the length of the encoded targets. Note that this function assumes
     the inputs and targets will have EOS appended and includes that in the reported length.
@@ -66,6 +68,7 @@ class UnsuperviseT5Dataset(Dataset):
     """This function is copy of https://github.com/IDEA-CCNL/Fengshenbang-LM/blob/
     ec13aeb8689cfafaa6a7a9e9595d110edbe34123/fengshen/data/t5_dataloader/t5_datasets.py#L61.
     """
+
     def __init__(self, data_path):
         # [{input_ids: ...}, {input_ids: ...}, ...]
         self.data = get_data(data_path)
