5d1387315f274735d2d54fd5435478cd58c811fe
xiezipeng <xzpaiks@163.com>
2022-08-02 04:05:55 +0000

    add comment

--- projects/T5/datasets/dataset.py
+++ projects/T5/datasets/dataset.py
@@ -17,6 +17,26 @@ def get_data(path):
 
 
 def compute_input_and_target_lengths(inputs_length, noise_density, mean_noise_span_length):
+    """This function is copy of `random_spans_helper <https://github.com/google-research/
+    text-to-text-transfer-transformer/blob/84f8bcc14b5f2c03de51bd3587609ba8f6bbd1cd/t5/data/preprocessors.py#L2466>`__ .
+    Training parameters to avoid padding with random_spans_noise_mask.
+    When training a model with random_spans_noise_mask, we would like to set the other
+    training hyperparmeters in a way that avoids padding.
+    This function helps us compute these hyperparameters.
+    We assume that each noise span in the input is replaced by extra_tokens_per_span_inputs sentinel tokens,
+    and each non-noise span in the targets is replaced by extra_tokens_per_span_targets sentinel tokens.
+    This function tells us the required number of tokens in the raw example (for split_tokens())
+    as well as the length of the encoded targets. Note that this function assumes
+    the inputs and targets will have EOS appended and includes that in the reported length.
+    Args:
+        inputs_length: an integer - desired length of the tokenized inputs sequence
+        noise_density: a float
+        mean_noise_span_length: a float
+    Returns:
+        tokens_length: length of original text in tokens
+        targets_length: an integer - length in tokens of encoded targets sequence
+    """
+
     def _tokens_length_to_inputs_length_targets_length(tokens_length):
         num_noise_tokens = int(round(tokens_length * noise_density))
         num_nonnoise_tokens = tokens_length - num_noise_tokens
@@ -43,6 +63,9 @@ def compute_input_and_target_lengths(inputs_length, noise_density, mean_noise_sp
 
 
 class UnsuperviseT5Dataset(Dataset):
+    """This function is copy of https://github.com/IDEA-CCNL/Fengshenbang-LM/blob/
+    ec13aeb8689cfafaa6a7a9e9595d110edbe34123/fengshen/data/t5_dataloader/t5_datasets.py#L61.
+    """
     def __init__(self, data_path):
         # [{input_ids: ...}, {input_ids: ...}, ...]
         self.data = get_data(data_path)
