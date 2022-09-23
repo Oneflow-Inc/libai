9a25af65f5d54337bc79831ac305a23d84fdc69d
CPFLAME <410070869@qq.com>
2022-09-23 09:51:52 +0000

    add logger info

--- projects/DALLE2/dalle2/dalle2_loader.py
+++ projects/DALLE2/dalle2/dalle2_loader.py
@@ -1,10 +1,18 @@
+import logging
+
 import omegaconf
 import oneflow as flow
 from oneflow.framework.check_point_v2 import _broadcast_py_object
 
 import libai.utils.distributed as dist
 from libai.models.build import build_model
-from libai.models.utils.model_utils.base_loader import ModelLoaderHuggerFace, _load_state_dict_into_model
+from libai.models.utils.model_utils.base_loader import (
+    ModelLoaderHuggerFace,
+    _load_state_dict_into_model,
+)
+
+logger = logging.getLogger("libai.dalle2." + __name__)
+
 
 class Dalle2ModelLoader(ModelLoaderHuggerFace):
     def __init__(self, model, libai_cfg, pretrained_model_path, **kwargs):
@@ -12,10 +20,10 @@ class Dalle2ModelLoader(ModelLoaderHuggerFace):
         self.base_model_prefix_1 = ""
         self.base_model_prefix_2 = ""
 
-    def _convert_state_dict(self, state_dict, module = 'prior'):
+    def _convert_state_dict(self, state_dict, module="prior"):
         old_keys = []
         new_keys = []
-        if module == 'prior':
+        if module == "prior":
             for k in state_dict.keys():
                 if "clip." in k:
                     continue
@@ -24,21 +32,21 @@ class Dalle2ModelLoader(ModelLoaderHuggerFace):
                     k = k[:-1] + "weight"
                 elif k.startswith("net.causal_transformer"):
                     if k.endswith("gamma"):
-                        k = k[:-5] + 'weight'
-                    elif k.endswith('beta'):
-                        k = k[:-4] + 'bias'
+                        k = k[:-5] + "weight"
+                    elif k.endswith("beta"):
+                        k = k[:-4] + "bias"
                 new_keys.append("prior." + k)
-        elif module == 'decoder':
+        elif module == "decoder":
             for k in state_dict.keys():
-                if 'clip.' in k:
+                if "clip." in k:
                     continue
                 old_keys.append(k)
                 if k.endswith(".g"):
                     k = k[:-1] + "weight"
-                elif 'cross_attn' in k:
-                    if k.endswith('gamma'):
+                elif "cross_attn" in k:
+                    if k.endswith("gamma"):
                         k = k[:-5] + "weight"
-                    elif k.endswith('beta'):
+                    elif k.endswith("beta"):
                         k = k[:-4] + "bias"
                 new_keys.append("decoder." + k)
         ret_state_dict = {}
@@ -46,28 +54,35 @@ class Dalle2ModelLoader(ModelLoaderHuggerFace):
             ret_state_dict[new_key] = state_dict.pop(old_key)
         return ret_state_dict
 
-
     def load(self):
         if dist.is_main_process():
-            #prior
-            torch_state_dict = self._load_torch_state_dict(self.libai_cfg.model.prior_weight_path)['ema_model']
+            # prior
+            logger.info("loading torch model prior...")
+            torch_state_dict = self._load_torch_state_dict(self.libai_cfg.model.prior_weight_path)[
+                "ema_model"
+            ]
+            logger.info("converting torch model prior into oneflow model...")
             flow_state_dict = self._convert_tensors(torch_state_dict)
             prior_state_dict = self._convert_state_dict(flow_state_dict)
-            #decoder
+            # decoder
+            logger.info("loading torch model decoder...")
             torch_state_dict = self._load_torch_state_dict(self.libai_cfg.model.decoder_weight_path)
             flow_state_dict = self._convert_tensors(torch_state_dict)
-            decoder_state_dict = self._convert_state_dict(flow_state_dict, module='decoder')
-            flow_state_dict = {**prior_state_dict, ** decoder_state_dict}
+            logger.info("converting torch model decoder into oneflow model...")
+            decoder_state_dict = self._convert_state_dict(flow_state_dict, module="decoder")
+            flow_state_dict = {**prior_state_dict, **decoder_state_dict}
         else:
             flow_state_dict = None
 
+        logger.info("building LiBai model...")
         self.libai_cfg = _broadcast_py_object(self.libai_cfg, src=0)
         self.model = build_model(self.model)
         self.model = self.model.eval()
-        
+
         flow.cuda.empty_cache()
         # State_dict to global
-        flow_state_dict = self._state_dict_to_global(flow_state_dict, mode="pytorch") #oom
+        logger.info("transfering state_dict local to global...")
+        flow_state_dict = self._state_dict_to_global(flow_state_dict, mode="pytorch")  # oom
         # Load
         # (
         #     model,
@@ -76,5 +91,6 @@ class Dalle2ModelLoader(ModelLoaderHuggerFace):
         #     mismatched_keys,
         #     error_msgs,
         # ) = self._load_pretrained_model(self.model, flow_state_dict, self.pretrained_model_path)
+        logger.info("loading model weights into LiBai...")
         _load_state_dict_into_model(self.model, flow_state_dict, "")
         return self.model
