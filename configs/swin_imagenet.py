d45b2abcc8e0b9c665df6654da0e4396b2837dbf
Ldpe2G <liangdepeng@gmail.com>
2022-09-25 14:28:00 +0000

    add clip grad

--- configs/swin_imagenet.py
+++ configs/swin_imagenet.py
@@ -29,6 +29,8 @@ model.cfg.loss_func = SoftTargetCrossEntropy()
 optim.lr = 1e-3
 optim.eps = 1e-8
 optim.weight_decay = 0.05
+optim.params.clip_grad_max_norm = 5.0
+optim.params.clip_grad_norm_type = 2.0
 
 # Refine train cfg for vit model
 train.train_micro_batch_size = 128
