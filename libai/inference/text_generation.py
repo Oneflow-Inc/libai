424545a813c863826ec4f1cf6ef82ddbf2c6d3af
CPFLAME <410070869@qq.com>
2022-10-12 07:53:38 +0000

    finish Mt5 pipeline generate

--- libai/inference/text_generation.py
+++ libai/inference/text_generation.py
@@ -94,14 +94,14 @@ if __name__ == "__main__":
         "projects/MT5/configs/t5_inference.py",
         data_parallel=1,
         tensor_parallel=1,
-        pipeline_parallel=1,
-        pipeline_stage_id=None,
+        pipeline_parallel=4,
+        pipeline_stage_id=[0]*6+[1]*6+[2]*6+[3]*6,
         pipeline_num_layers=12 * 2,
         model_path="data_test/t5_inference_model",
         mode="huggingface",
     )
 
     text = ["summarize: She is a student, She is tall, She loves study"]
-    dict1 = pipeline(text)
+    dict1 = pipeline(text, do_sample=False, num_beams=3)
     if dist.is_main_process():
         print(dict1)
