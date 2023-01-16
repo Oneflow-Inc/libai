611d8688b695dd17e4fe76e913cc2966a498f532
xiezipeng-ML <xzpaiks@163.com>
2023-01-16 03:58:31 +0000

    update glm_loader_libai

--- projects/GLM/utils/glm_loader.py
+++ projects/GLM/utils/glm_loader.py
@@ -103,3 +103,56 @@ class GLMLoaderLiBai(ModelLoaderLiBai):
     def __init__(self, model, libai_cfg, pretrained_model_path, **kwargs):
         super().__init__(model, libai_cfg, pretrained_model_path, **kwargs)
         self.base_model_prefix_2 = "glm"
+
+    def _convert_state_dict(self, flow_state_dict, cfg):
+        """Convert state_dict's keys to match model.
+
+        Args:
+            flow_state_dict (OrderedDict): model state dict.
+            cfg (dict): model's default config dict in LiBai.
+
+        Returns:
+            OrderedDict: flow state dict.
+        """
+        # The converted checkpoint.
+        oneflow_state_dict = flow_state_dict.copy()
+        old_keys = list(oneflow_state_dict.keys())
+
+        # Get configs
+        num_heads = cfg.get("num_attention_heads")
+        hidden_size = cfg.get("hidden_size")
+        head_size = int(hidden_size / num_heads)
+
+        # prefix
+        has_prefix = any(s.startswith(self.base_model_prefix_1) for s in oneflow_state_dict)
+        prefix1 = self.base_model_prefix_1 + "." if has_prefix else ""
+        prefix2 = "glm." if has_prefix else ""
+
+        # Convert Embedding layers.
+        new_key = prefix2 + "embeddings.word_embeddings.weight"
+        old_keys.remove(prefix1 + "word_embeddings.weight")
+        oneflow_state_dict[new_key] = oneflow_state_dict.pop(prefix1 + "word_embeddings.weight")
+
+        if cfg.get("block_position_encoding", False) == True:
+            new_key = prefix2 + "embeddings.position_embeddings.weight"
+            old_keys.remove(prefix1 + "transformer.position_embeddings.weight")
+            oneflow_state_dict[new_key] = oneflow_state_dict.pop(
+                prefix1 + "transformer.position_embeddings.weight"
+            )
+
+            new_key = prefix2 + "embeddings.block_position_embeddings.weight"
+            old_keys.remove(prefix1 + "transformer.block_position_embeddings.weight")
+            oneflow_state_dict[new_key] = oneflow_state_dict.pop(
+                prefix1 + "transformer.block_position_embeddings.weight"
+            )
+
+        # Convert other layers.
+        for key in old_keys:
+            if "query_key_value" in key:
+                qkv = oneflow_state_dict.pop(key)
+                qkv = self._fix_qkv_ordering(qkv, head_size, num_heads)
+                oneflow_state_dict[prefix2 + key] = qkv
+            else:
+                oneflow_state_dict[prefix2 + key] = oneflow_state_dict.pop(key)
+
+        return oneflow_state_dict
