a9747f411cf4b0553d07f03a544c37c1552d4549
Juncheng <liujuncheng1022@gmail.com>
2023-03-03 10:07:07 +0800

    Add knob ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT (#466)
    * Add knob LIBAI_LINEAR_SKIP_INIT
    
    * ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT
    
    * fix import
    
    * add env in infer.sh
    
    * add fp16 initial
    
    ---------
    
    Co-authored-by: CPFLAME <410070869@qq.com>

--- tools/infer.sh
+++ tools/infer.sh
@@ -7,6 +7,8 @@ NODE_RANK=${NODE_RANK:-0}
 ADDR=${ADDR:-127.0.0.1}
 PORT=${PORT:-12345}
 
+export ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT=1
+
 python3 -m oneflow.distributed.launch \
 --nproc_per_node $GPUS --nnodes $NODE --node_rank $NODE_RANK --master_addr $ADDR --master_port $PORT \
 $FILE ${@:3}
