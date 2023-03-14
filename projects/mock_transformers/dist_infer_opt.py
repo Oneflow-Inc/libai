ea60eabd1e914602b64e1be83238fb389e7bcc4f
liujuncheng <liujuncheng1022@gmail.com>
2023-03-14 10:35:28 +0800

    use fused_attention_concat_past_key_value

--- projects/mock_transformers/dist_infer_opt.py
+++ projects/mock_transformers/dist_infer_opt.py
@@ -71,21 +71,18 @@ class LiBaiOPTAttention(temp_class):
             [self.q_proj.weight, self.k_proj.weight, self.v_proj.weight],
             [self.q_proj.bias, self.k_proj.bias, self.v_proj.bias],
         )
-        if past_key_value is not None:
-            key_states, value_states = flow._C.fused_attention_concat_past_key_value(
-                past_key=past_key_value[0],
-                past_key_layout="BHMK",
-                past_value=past_key_value[1],
-                past_value_layout="BHMK",
-                key=key_states,
-                key_layout="BM(HK)",
-                value=value_states,
-                value_layout="BM(HK)",
-                key_head_size=self.head_dim,
-            )
-        else:
-            key_states = self._shape(key_states, -1, bsz)
-            value_states = self._shape(value_states, -1, bsz)
+        past_key, past_value = (past_key_value[0], past_key_value[1]) if past_key_value is not None else (None, None)
+        key_states, value_states = flow._C.fused_attention_concat_past_key_value(
+            past_key=past_key,
+            past_key_layout="BHMK",
+            past_value=past_value,
+            past_value_layout="BHMK",
+            key=key_states,
+            key_layout="BM(HK)",
+            value=value_states,
+            value_layout="BM(HK)",
+            key_head_size=self.head_dim,
+        )
 
         past_key_value = (key_states, value_states)
 
