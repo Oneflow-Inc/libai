d235bc1d16f51ff001f33896b0fa9adf1ea179d3
xiezipeng-ML <xzpaiks@163.com>
2023-03-14 08:00:09 +0000

    refine

--- configs/common/data/t5_dataset.py
+++ configs/common/data/t5_dataset.py
@@ -33,7 +33,7 @@ dataloader.train = LazyCall(build_nlp_train_val_test_loader)(
             name="t5",
             data_prefix="/workspace/data/libai_dataset/loss_compara_content_sentence",
             indexed_dataset=LazyCall(get_indexed_dataset)(
-                data_prefix="/workspace/data/libai_dataset/" "/loss_compara_content_sentence",
+                data_prefix="/workspace/data/libai_dataset/loss_compara_content_sentence",
                 data_impl="mmap",
                 skip_warmup=False,
             ),
@@ -49,3 +49,24 @@ dataloader.train = LazyCall(build_nlp_train_val_test_loader)(
     weights=[1.0],
     num_workers=4,
 )
+
+dataloader.test = [LazyCall(build_nlp_test_loader)(
+    dataset=
+        LazyCall(T5Dataset)(
+            name="t5",
+            data_prefix="/workspace/data/libai_dataset/loss_compara_content_sentence",
+            indexed_dataset=LazyCall(get_indexed_dataset)(
+                data_prefix="/workspace/data/libai_dataset/loss_compara_content_sentence",
+                data_impl="mmap",
+                skip_warmup=False,
+            ),
+            max_num_samples=10,
+            max_seq_length=512,
+            max_seq_length_dec=128,
+            masked_lm_prob=0.15,
+            short_seq_prob=0.1,
+            seed=1234,
+        ),
+        test_batch_size=4,
+    )
+]
