347d6d1fbadd6455878da9cb1dba2e0bc60a60b3
xiezipeng-ML <xzpaiks@163.com>
2023-04-11 10:34:12 +0000

    refine

--- projects/mock_transformers/mock_tokenization.py
+++ projects/mock_transformers/mock_tokenization.py
@@ -13,20 +13,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import oneflow as flow
 
 from libai.utils import distributed as dist
 
 flow.mock_torch.enable()
 
-from transformers import BertTokenizer, GPT2Tokenizer, MT5Tokenizer, T5Tokenizer
-from transformers.tokenization_utils_base import *
-from transformers.utils import generic
-from transformers.utils.generic import TensorType
+from transformers import BertTokenizer, GPT2Tokenizer, MT5Tokenizer, T5Tokenizer  # noqa
+from transformers.tokenization_utils_base import *  # noqa
+from transformers.utils import generic  # noqa
+from transformers.utils.generic import TensorType  # noqa
 
 
 # ---------------- mock TensorType ------------------
-class TensorType(ExplicitEnum):
+class TensorType(ExplicitEnum):  # noqa
     PYTORCH = "pt"
     TENSORFLOW = "tf"
     ONEFLOW = "of"
@@ -49,16 +51,17 @@ def flow_convert_to_tensors(self, tensor_type=None, prepend_batch_axis=False):
     is_tensor = None
     # Get a function reference for the correct framework
     if tensor_type == TensorType.TENSORFLOW:
-        if not is_tf_available():
+        if not is_tf_available():  # noqa
             raise ImportError(
-                "Unable to convert output to TensorFlow tensors format, TensorFlow is not installed."
+                "Unable to convert output to TensorFlow tensors format, TensorFlow is not "
+                "installed."
             )
         import tensorflow as tf
 
         as_tensor = tf.constant
         is_tensor = tf.is_tensor
     elif tensor_type == TensorType.PYTORCH:
-        if not is_torch_available():
+        if not is_torch_available():  # noqa
             raise ImportError(
                 "Unable to convert output to PyTorch tensors format, PyTorch is not installed."
             )
@@ -68,25 +71,24 @@ def flow_convert_to_tensors(self, tensor_type=None, prepend_batch_axis=False):
         is_tensor = torch.is_tensor
     elif tensor_type == TensorType.ONEFLOW:
         try:
-            import oneflow
-        except:
-            raise ImportError(
-                "Unable to convert output to OneFlow tensors format, OneFlow is not installed."
-            )
+            import oneflow  # noqa
+        except ImportError as e:
+            msg = "Unable to convert output to OneFlow tensors format, OneFlow is not installed."
+            raise ImportError(msg) from e
         as_tensor = flow.tensor
         is_tensor = flow.is_tensor
     elif tensor_type == TensorType.JAX:
-        if not is_flax_available():
+        if not is_flax_available():  # noqa
             raise ImportError(
                 "Unable to convert output to JAX tensors format, JAX is not installed."
             )
         import jax.numpy as jnp  # noqa: F811
 
         as_tensor = jnp.array
-        is_tensor = is_jax_tensor
+        is_tensor = is_jax_tensor  # noqa
     else:
-        as_tensor = np.asarray
-        is_tensor = is_numpy_array
+        as_tensor = np.asarray  # noqa
+        is_tensor = is_numpy_array  # noqa
 
     # Do the tensor conversion in batch
     for key, value in self.items():
@@ -109,15 +111,16 @@ def flow_convert_to_tensors(self, tensor_type=None, prepend_batch_axis=False):
             if key == "overflowing_tokens":
                 raise ValueError(
                     "Unable to create tensor returning overflowing tokens of different lengths. "
-                    "Please see if a fast version of this tokenizer is available to have this feature available."
+                    "Please see if a fast version of this tokenizer is available to have this "
+                    "feature available."
                 ) from e
             raise ValueError(
-                "Unable to create tensor, you should probably activate truncation and/or padding with"
-                " 'padding=True' 'truncation=True' to have batched tensors with the same length. Perhaps your"
-                f" features (`{key}` in this case) have excessive nesting (inputs type `list` where type `int` is"
-                " expected)."
+                "Unable to create tensor, you should probably activate truncation and/or "
+                "padding with 'padding=True' 'truncation=True' to have batched tensors with "
+                f"the same length. Perhaps your features (`{key}` in this case) have "
+                "excessive nesting (inputs type `list` where type `int` is expected)."
             ) from e
-    if os.getenv("IS_GLOBAL", True) == True:
+    if os.getenv("IS_GLOBAL", True) is True:
         size = self["input_ids"].size()
         sbp = dist.get_nd_sbp([flow.sbp.broadcast, flow.sbp.broadcast])
 
@@ -134,4 +137,4 @@ def flow_convert_to_tensors(self, tensor_type=None, prepend_batch_axis=False):
     return self
 
 
-BatchEncoding.convert_to_tensors = flow_convert_to_tensors
+BatchEncoding.convert_to_tensors = flow_convert_to_tensors  # noqa
