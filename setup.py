c92e60b942a161bdb7aeeafb9189f11e3204f86d
xiezipeng-ML <xzpaiks@163.com>
2023-12-11 23:32:19 +0800

    test

--- setup.py
+++ setup.py
@@ -46,12 +46,12 @@ if sys.version_info < (3,):
 
 
 def get_pybind11():
-    # try:
-    #     import pybind11 as pb
-    # except ImportError:
-    #     print("pybind11 not found, installing...")
-    #     pip.main(["install","pybind11"])
-    import pybind11 as pb
+    try:
+        import pybind11 as pb
+    except ImportError:
+        print("pybind11 not found, installing...")
+        os.system("python -m pip install pybind11")
+        import pybind11 as pb
     return pb
 
 
