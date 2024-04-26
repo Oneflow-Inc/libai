8b0d41b50b4cc848530ff2a1fa081d0163b566ce
xiezipeng-ML <xzpaiks@163.com>
2024-04-26 15:15:57 +0800

    refine

--- projects/libai-parallel-case/configs/gpt2_pretrain_tensor_parallel.py
+++ projects/libai-parallel-case/configs/gpt2_pretrain_tensor_parallel.py
@@ -21,11 +21,6 @@ dataloader.test[0].dataset.indexed_dataset.data_prefix = data_prefix
 # GPT-2 model config
 model.cfg.embedding_dropout_prob = 0.1
 model.cfg.attention_dropout_prob = 0.1
-model.cfg.num_attention_heads = 25
-model.cfg.hidden_size = 1600
-model.cfg.ffn_hidden_size = 1024
-model.cfg.hidden_layers = 48
-model.cfg.max_seq_length = 1024
 
 train.input_placement_device = "cpu"
 
