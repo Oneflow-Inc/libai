d7795020d4af756b2d594dbee5ffdce2e72b0b53
ShawnXuan <xiexuanx2@gmail.com>
2024-09-14 14:08:25 +0800

    add Aquila

---
+++ projects/Aquila/configs/aquila_sft.py
@@ -0,0 +1,102 @@
+import os
+from omegaconf import OmegaConf
+
+from libai.config import LazyCall
+from libai.evaluation import PPLEvaluator
+from libai.scheduler import WarmupExponentialLR
+from libai.data.build import build_nlp_train_loader, build_nlp_test_loader
+
+from configs.train import train
+from configs.graph import graph
+from configs.optim import optim
+
+from projects.Aquila.aquila import AquilaForCausalLM
+from projects.Aquila.tokenizer import AquilaTokenizer
+from projects.Aquila.configs.aquila_config import cfg
+from projects.Aquila.aquila_dataset import AquilaDataset
+
+
+# Hyperparameters
+weight_decay = 0.1
+learning_rate = 5e-5
+dataset_path = "./data/libai_xpu_alpaca"
+pretrained_model_path = "/root/models/Aquila-7B"
+
+# graph & optim
+graph["enabled"] = False
+optim.update(
+    dict(
+        lr=learning_rate,
+        weight_decay=weight_decay,
+    )
+)
+
+# tokenize
+tokenization = OmegaConf.create()
+tokenization.make_vocab_size_divisible_by = 1
+tokenization.tokenizer = LazyCall(AquilaTokenizer)(
+    vocab_file=pretrained_model_path+"/vocab.json",
+    merges_file=pretrained_model_path+"/merges.txt",
+)
+
+
+# model
+cfg.pretrained_model_path = pretrained_model_path
+model = LazyCall(AquilaForCausalLM)(cfg=cfg)
+
+# datasets
+dataloader = OmegaConf.create()
+dataloader.train = LazyCall(build_nlp_train_loader)(
+    dataset=[
+        LazyCall(AquilaDataset)(
+            path=os.path.join(dataset_path, "train"), tokenizer=tokenization.tokenizer
+        )
+    ],
+)
+dataloader.test = [
+    LazyCall(build_nlp_test_loader)(
+        dataset=LazyCall(AquilaDataset)(
+            path=os.path.join(dataset_path, "test"), tokenizer=tokenization.tokenizer
+        ),
+    ),
+]
+
+train.update(
+    dict(
+        output_dir="./sft_result",
+        train_micro_batch_size=4,
+        test_micro_batch_size=1,
+        train_epoch=5,
+        train_iter=1,
+        log_period=1,
+        warmup_ratio=1 / 3,
+        num_accumulation_steps=8,
+        rdma_enabled=False,
+        train_with_fp16=True,
+        amp=dict(enabled=True),
+        activation_checkpoint=dict(enabled=True),
+        input_placement_device='xpu',
+        checkpointer=dict(
+            period=100,
+            max_to_keep=20,
+        ),
+        dist=dict(
+            data_parallel_size=1,
+            tensor_parallel_size=1,
+            pipeline_parallel_size=1,
+            pipeline_num_layers=cfg.hidden_layers,
+            device_type='xpu',
+        ),
+        evaluation=dict(
+            enabled=False,
+            evaluator=LazyCall(PPLEvaluator)(),
+            eval_period=1000,
+            eval_iter=1e5,
+        ),
+        scheduler=LazyCall(WarmupExponentialLR)(
+            warmup_factor=0.0,
+            gamma=1.0,
+            warmup_method="linear",
+        ),
+    )
+)
---
+++ projects/Aquila/pipeline.py
@@ -0,0 +1,149 @@
+# coding=utf-8
+# Copyright 2021 The OneFlow Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import click
+
+from libai.inference.basic import BasePipeline
+from libai.utils import distributed as dist
+
+
+class TextGenerationPipeline(BasePipeline):
+    def load_pretrain_weight(self, libai_cfg_model, model_path, mode="huggingface"):
+        """load pretrained model.
+
+        Args:
+            libai_cfg_model (libai.models): Lazy config Model in Libai, you can import it
+                by `from libai.config.configs.common.models.bert
+                    import pretrain_model as libai_cfg_model`
+            model_path (str): The directory path of pretrained model,
+        """
+        if mode == "huggingface":
+            from projects.Aquila.utils.aquila_loader import AquilaLoaderHuggerFace
+
+            model_loader = AquilaLoaderHuggerFace(
+                libai_cfg_model,
+                libai_cfg_model.cfg,
+                model_path,
+            )
+            model = model_loader.load()
+            model.eval()
+            return model
+
+        elif mode == "libai":
+            from projects.Aquila.utils.aquila_loader import AquilaLoaderLiBai
+
+            model_loader = AquilaLoaderLiBai(
+                libai_cfg_model,
+                libai_cfg_model.cfg,
+                model_path,
+            )
+            model = model_loader.load()
+            model.eval()
+            return model
+
+        elif mode == "random":
+            from libai.engine import DefaultTrainer
+
+            return DefaultTrainer.build_model(self.cfg)
+        else:
+            raise NotImplementedError
+
+    def _parse_parameters(self, **pipeline_parameters):
+        preprocess_params = {}
+        forward_params = {**pipeline_parameters}
+        postprocess_params = {}
+
+        return preprocess_params, forward_params, postprocess_params
+
+    def preprocess(self, inputs, **kwargs) -> dict:
+        # tokenizer encoderW
+        import oneflow as flow
+        inputs = flow.tensor(self.tokenizer.encode(inputs, add_bos=True, padding=True))
+
+        inputs = {
+            "input_ids": inputs,
+        }
+
+        return inputs
+
+    def forward(self, inputs, **kwargs) -> dict:
+        inputs = dist.convert_to_distributed_default_setting(inputs["input_ids"])
+        outputs = self.model.generate(inputs, max_length=50, **kwargs)
+        return {"return_ids": outputs}
+
+    def postprocess(self, model_output_dict, **kwargs) -> dict:
+        return_ids = model_output_dict["return_ids"]
+
+        records = [
+            {"generated_text": self.tokenizer.decode(return_ids[i])}
+            for i in range(return_ids.size(0))
+        ]
+        return records
+
+    def build_tokenizer(self, cfg):
+        tokenizer = None
+        if try_get_key(cfg, "tokenization") is not None:
+            tokenizer_cfg = cfg.tokenization.tokenizer
+            if "vocab_file" not in tokenizer_cfg:
+                # If "vocab_file" does not exist in the tokenizer's config,
+                # set it to default as f"{model_path}/tokenizer.model"
+                tokenizer_cfg.vocab_file = str(
+                    Path(self.model_path).joinpath("vocab.json")
+                )
+            if "merges_file" not in tokenizer_cfg:
+                # If "merges_file" does not exist in the tokenizer's config,
+                # set it to default as f"{model_path}/tokenizer.model"
+                tokenizer_cfg.vocab_file = str(
+                    Path(self.model_path).joinpath("merges.txt")
+                )
+            tokenizer = DefaultTrainer.build_tokenizer(cfg)
+        return tokenizer
+
+
+@click.command()
+@click.option(
+    "--config_file",
+    default="projects/Aquila/configs/aquila_config.py",
+    help="Path to the configuration file.",
+)
+@click.option("--model_path", default=None, help="Path to the model checkpoint.")
+@click.option(
+    "--mode",
+    default="libai",
+    help="Mode for the dataloader pipeline, e.g., 'libai' or 'huggingface'.",
+)
+@click.option(
+    "--device", default="cuda", help="Device to run the model on, e.g., 'cuda', 'xpu', 'npu'."
+)
+def main(config_file, model_path, mode, device):
+    pipeline = TextGenerationPipeline(
+        config_file,
+        data_parallel=1,
+        tensor_parallel=1,
+        pipeline_parallel=1,
+        pipeline_num_layers=32,
+        model_path=model_path, #'/root/models/Aquila-7B',
+        mode=mode,
+        device=device,
+    )
+
+    text = [
+        "Give three tips for staying healthy.",
+    ]
+    output = pipeline(inputs=text)
+    if dist.is_main_process():
+        print(output)
+
+if __name__ == "__main__":
+    main()
