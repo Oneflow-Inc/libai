720e3d4e28c67eb76cd5ae20f1204d990d30be99
ShawnXuan <xiexuanx2@gmail.com>
2025-01-20 03:02:51 +0000

    gpt batchsize to 4

--- graph_train_npu.sh
+++ graph_train_npu.sh
@@ -2,17 +2,18 @@ DP=${1:-1}
 TP=${2:-1}
 PP=${3:-1}
 NUM_DEVICES=$(( DP * TP * PP ))
+export ASCEND_RT_VISIBLE_DEVICES=4,5,6,7
 #export ASCEND_RT_VISIBLE_DEVICES=4,5,6,7
 #export ASCEND_SLOG_PRINT_TO_STDOUT=1
-#export ASCEND_GLOBAL_LOG_LEVEL=0
+#export ASCEND_GLOBAL_LOG_LEVEL=3
 #export ONEFLOW_DEBUG=1
-export ONEFLOW_ENABLE_MULTI_TENSOR_MODEL_UPDATE=1
+export ONEFLOW_ENABLE_MULTI_TENSOR_MODEL_UPDATE=0
 python3 -m oneflow.distributed.launch \
     --nproc_per_node $NUM_DEVICES \
     --nnodes 1 \
     --node_rank 0 \
     --master_addr 127.0.0.1 \
-    --master_port 12345 \
+    --master_port 18245 \
         tools/train_net.py --config-file=configs/gpt2_pretrain.py \
             graph.enabled=True \
 	    optim.fused=True \
@@ -27,9 +28,9 @@ python3 -m oneflow.distributed.launch \
             model.cfg.scale_mask_softmax_fusion=False \
 	    model.cfg.embedding_dropout_prob=0.0 \
             model.cfg.attention_dropout_prob=0.0 \
+	    train.train_micro_batch_size=4 \
 	    train.train_iter=10 \
             model.cfg.bias_gelu_fusion=False
-	    #train.train_micro_batch_size=2 \
         #tools/train_net.py --config-file=configs/gpt2_pretrain.py \
         #tools/train_net.py --config-file=projects/libai-parallel-case/configs/gpt2_pretrain_data_parallel.py \
 	    #train.load_weight=init_1b_ckpt \
