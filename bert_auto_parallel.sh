fb461b2b469cd88f5ee810df30a0a3aaebf3a67b
ShawnXuan <xiexuanx2@gmail.com>
2025-04-22 15:56:13 +0800

    bash scripts for test

---
+++ bert_auto_parallel.sh
@@ -0,0 +1,33 @@
+#export ASCEND_RT_VISIBLE_DEVICES=4,5,6,7
+#export ASCEND_SLOG_PRINT_TO_STDOUT=1
+#export ASCEND_GLOBAL_LOG_LEVEL=2
+#export ONEFLOW_DEBUG=1
+#export ONEFLOW_ENABLE_MULTI_TENSOR_MODEL_UPDATE=0
+#export ONEFLOW_NPU_COMM_SYNC=1
+python3 -m oneflow.distributed.launch \
+    --nproc_per_node 8 \
+    --nnodes 1 \
+    --node_rank 0 \
+    --master_addr 127.0.0.1 \
+    --master_port 18245 \
+        tools/train_net.py --config-file=configs/bert_large_pretrain_ap.py \
+            graph.enabled=True \
+            optim.fused=True \
+            train.input_placement_device="npu" \
+            train.dist.device_type="npu" \
+            train.amp.enabled=False \
+            train.evaluation.enabled=False \
+            train.log_period=1 \
+            model.cfg.hidden_dropout_prob=0.0 \
+            model.cfg.attention_probs_dropout_prob=0.0 \
+	    model.cfg.add_binary_head=False \
+            train.train_micro_batch_size=32 \
+            train.train_iter=10 \
+            model.cfg.bias_dropout_fusion=False \
+            model.cfg.scale_mask_softmax_fusion=False \
+            model.cfg.bias_gelu_fusion=False
+            #train.train_iter=10 \
+            #train.load_weight=init_1b_ckpt \
+            #train.num_accumulation_steps=2 \
+            #train.train_micro_batch_size=1 \
+            #train.num_accumulation_steps=2 \
---
+++ llama_train_npu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 模型规模（如 7b, 13b）
+MODEL_SIZE=${1:-7b}
+
+# 是否启用自动并行（1 = 是，0 = 否）
+AUTO_PARALLEL=${2:-1}
+
+export PRETRAINED_MODEL_PATH=meta-llama/Llama-2-${MODEL_SIZE}-hf
+
+if [ "$AUTO_PARALLEL" -eq 1 ]; then
+    CONFIG_FILE=projects/Llama/configs/llama_sft_ap.py
+else
+    CONFIG_FILE=projects/Llama/configs/llama_sft.py
+fi
+
+#export ONEFLOW_NPU_COMM_SYNC=1
+python3 -m oneflow.distributed.launch \
+    --nproc_per_node 8 \
+    --nnodes 1 \
+    --node_rank 0 \
+    --master_addr 127.0.0.1 \
+    --master_port 18245 \
+    projects/Llama/train_net.py \
+        --config-file=${CONFIG_FILE} \
+        graph.enabled=True \
+        train.input_placement_device="npu" \
+        train.dist.device_type="npu" \
+        train.amp.enabled=False \
+	train.num_accumulation_steps=1 \
+	train.train_epoch=0 \
+	train.train_iter=20 \
+	train.log_period=1
+    #tools/train_net.py \
