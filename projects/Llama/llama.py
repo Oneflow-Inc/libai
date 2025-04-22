2ffd7585deb7e2e06e88a1c5291ec65673fc68ab
ShawnXuan <xiexuanx2@gmail.com>
2025-04-22 15:54:25 +0800

    replace cross_entropy with sparse_softmax_cross_entropy

--- projects/Llama/llama.py
+++ projects/Llama/llama.py
@@ -503,8 +503,9 @@ class CrossEntropyLoss(nn.Module):
         target = target.to_global(placement=logits.placement)
         target = target * (target >= 0)
 
-        lm_loss = flow._C.cross_entropy(
-            logits.view(-1, logits.shape[-1]), target.view(-1), ignore_index=0
+        #lm_loss = flow._C.cross_entropy(
+        lm_loss = flow._C.sparse_softmax_cross_entropy(
+            logits.view(-1, logits.shape[-1]), target.view(-1)#, ignore_index=0
         )
         return lm_loss
 
