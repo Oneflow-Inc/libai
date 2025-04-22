61a53c8465956d909844c3ed1c35ea3f3807a9a2
ShawnXuan <xiexuanx2@gmail.com>
2025-04-22 15:56:55 +0800

    all test sh

---
+++ all.sh
@@ -0,0 +1,13 @@
+bash gpt_train_npu.sh 1 1 1 2>&1 | tee log/gpt_111.log
+bash gpt_train_npu.sh 8 1 1 2>&1 | tee log/gpt_811.log
+bash gpt_train_npu.sh 1 8 1 2>&1 | tee log/gpt_181.log
+bash gpt_train_npu.sh 1 1 8 2>&1 | tee log/gpt_118.log
+bash gpt_auto_parallel.sh 2>&1 | tee log/gpt_ap.log
+bash bert_train_npu.sh 8 1 1 2>&1 | tee log/bert_811.log
+bash bert_train_npu.sh 1 8 1 2>&1 | tee log/bert_181.log
+bash bert_train_npu.sh 1 1 8 2>&1 | tee log/bert_118.log
+bash bert_auto_parallel.sh 2>&1 | tee log/bert_ap.log
+bash llama_train_npu.sh 7b 0 2>&1 | tee log/llama_7b_pp.log
+bash llama_train_npu.sh 13b 0 2>&1 | tee log/llama_13b_pp.log
+bash llama_train_npu.sh 7b 1 2>&1 | tee log/llama_7b_ap.log
+bash llama_train_npu.sh 13b 1 2>&1 | tee log/llama_13b_ap.log
