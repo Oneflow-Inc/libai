240778c4a6f89334b53cf6dc17af2372ae0a6f89
ShawnXuan <xiexuanx2@gmail.com>
2025-05-07 17:41:24 +0800

    fix

--- libai/engine/default.py
+++ libai/engine/default.py
@@ -280,7 +280,7 @@ class DefaultTrainer(TrainerBase):
             cfg.train.amp = dict(enabled=False)
             cfg.train.activation_checkpoint = dict(enabled=True)
 
-        if cfg.graph["auto_parallel"]["enabled"] == True and dist.get_world_size() == 8: # and (cfg.train.dist["data_parallel_size"] != 8):
+        if 0 and cfg.graph["auto_parallel"]["enabled"] == True and dist.get_world_size() == 8: # and (cfg.train.dist["data_parallel_size"] != 8):
             # cfg.train.dist["data_parallel_size"] = 8
             # cfg.train.dist["tensor_parallel_size"] = 1 
             # cfg.train.dist["pipeline_parallel_size"] = 1
