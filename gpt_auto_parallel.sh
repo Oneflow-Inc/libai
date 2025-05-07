260d9c75652d11b8c67fa0aa5401707876837495
ShawnXuan <xiexuanx2@gmail.com>
2025-05-07 17:41:01 +0800

    gpt auto parallel

--- gpt_auto_parallel.sh
+++ gpt_auto_parallel.sh
@@ -1,5 +1,5 @@
 NUM_DEVICES=${1:-8}
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+# export ASCEND_RT_VISIBLE_DEVICES=4,5,6,7
 # export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3
 #export ASCEND_SLOG_PRINT_TO_STDOUT=1
 #export ASCEND_GLOBAL_LOG_LEVEL=3
@@ -18,6 +18,7 @@ export ONEFLOW_ENABLE_MULTI_TENSOR_MODEL_UPDATE=0
 #export GLOG_log_dir=./logs
 #export GLOG_v=5
 #export GLOG_logtostderr=1
+export AUTO_MEMORY_MODE=HeavyMemoryDown
 bash tools/train.sh tools/train_net.py \
     projects/libai-parallel-case/configs/gpt2_pretrain_auto_parallel.py $NUM_DEVICES \
     train.log_period=1 \
@@ -25,7 +26,7 @@ bash tools/train.sh tools/train_net.py \
     optim.fused=True \
     train.dist.device_type="npu" \
     train.input_placement_device="npu" \
-    train.train_micro_batch_size=2 \
+    train.train_micro_batch_size=4 \
     train.train_iter=10 \
     train.amp.enabled=False \
     model.cfg.scale_mask_softmax_fusion=False \
